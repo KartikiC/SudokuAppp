2736d0ac0725b8ffbcbe196b3ce32cfde06b0d5a
Kartiki <chavankartiki@gmail.com>
2018-03-08 12:22:20 +0530

    Initial commit

---
+++ app/src/main/java/kartiki/example/com/sudokuapp/MainActivity.java
@@ -0,0 +1,33 @@
+package kartiki.example.com.sudokuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+            int[][] array2Da = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+            {5, 2, 0, 0, 0, 0, 0, 0, 0},
+            {0, 8, 7, 0, 0, 0, 0, 3, 1},
+            {0, 0, 3, 0, 1, 0, 0, 8, 0},
+            {9, 0, 0, 8, 6, 3, 0, 0, 5},
+            {0, 5, 0, 0, 9, 0, 6, 0, 0},
+            {1, 3, 0, 0, 0, 0, 2, 5, 0},
+            {0, 0, 0, 0, 0, 0, 0, 7, 4},
+            {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+
+
+        Sudoku s = new Sudoku(array2Da);
+        s.solveSudoku();
+        s.displaySudoku();
+
+    }
+
+}
